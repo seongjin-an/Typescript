fd7cc85a704b8a810382f0afe0dca09d7469fadf
seongjin-an <seongjin4us@gmail.com>
2021-04-30 00:29:12 +0900

    config2

---
+++ basic/src/person/Person.ts
@@ -0,0 +1,14 @@
+let MAX_AGE=100
+
+export interface IPerson{
+    name:string,
+    age:number
+}
+
+export class Person implements IPerson{
+    constructor(public name:string, public age:number){}
+}
+function makeRandomNumber(max:number=MAX_AGE):number{
+    return Math.ceil((Math.random()*max))
+}
+export const makePerson = (name:string, age:number = makeRandomNumber()) => ({name, age})
